ea426dec08d8b7aec4022ed0787a0086d0f94664
Markus Klein <markus-klein@live.de>
2020-02-28 00:13:25 +0100

    formatting

--- python_client/throttle_client/__init__.py
+++ python_client/throttle_client/__init__.py
@@ -13,7 +13,10 @@ class Lease:
     """
 
     def __init__(
-        self, id: str, active: Dict[str, int] = {}, pending: Dict[str, int] = {},
+        self,
+        id: str,
+        active: Dict[str, int] = {},
+        pending: Dict[str, int] = {},
     ):
         self.id = id
         self.active = active
@@ -69,14 +72,20 @@ class Client:
             "pending": {semaphore: amount},
             "valid_for_sec": valid_for_sec,
         }
-        response = requests.post(self.base_url + "/acquire", json=body, timeout=30)
+        response = requests.post(
+            self.base_url + "/acquire", json=body, timeout=30
+        )
         _raise_for_status(response)
-        if response.status_code == 201:  # Created. We got a lease to the semaphore
+        if (
+            response.status_code == 201
+        ):  # Created. We got a lease to the semaphore
             return Lease(id=response.text, active={semaphore: amount})
         elif response.status_code == 202:  # Accepted. Ticket pending.
             return Lease(id=response.text, pending={semaphore: amount})
 
-    @backoff.on_exception(backoff.expo, (requests.ConnectionError, requests.Timeout))
+    @backoff.on_exception(
+        backoff.expo, (requests.ConnectionError, requests.Timeout)
+    )
     def wait_for_admission(self, lease: Lease, timeout_ms: int = 0):
         """
         Check if the lease is still pending. An optional timeout allows to
@@ -115,7 +124,9 @@ class Client:
         number could become negative, if the semaphores have been overcommitted
         (due to previously reoccuring leases previously considered dead).
         """
-        response = requests.get(self.base_url + f"/remainder?semaphore={semaphore}")
+        response = requests.get(
+            self.base_url + f"/remainder?semaphore={semaphore}"
+        )
         _raise_for_status(response)
 
         return int(response.text)
@@ -151,12 +162,16 @@ class Client:
         Sends a PUT request to the server, updating the expiration timestamp
         and repeating the information in the lease.
         """
+
         assert (
             not lease.has_pending()
         ), "Pending leases must not be kept alive via heartbeat."
         response = requests.put(
             f"{self.base_url}/leases/{lease.id}",
-            json={"valid_for_sec": self.expiration_time_sec, "active": lease.active,},
+            json={
+                "valid_for_sec": self.expiration_time_sec,
+                "active": lease.active,
+            },
             timeout=30,
         )
         _raise_for_status(response)
