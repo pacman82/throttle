4ba94fe2a79077e308753636c560c7beebdf0320
Markus Klein <markus-klein@live.de>
2020-03-06 21:06:32 +0100

    formatting

--- src/state.rs
+++ src/state.rs
@@ -206,24 +206,22 @@ impl State {
 }
 
 lazy_static! {
-    static ref FULL_COUNT: IntGaugeVec =
-        register_int_gauge_vec!(
-            "throttle_full_count",
-            "New leases which would increase the count beyond this limit are pending.",
-            &["semaphore"]
-        ).expect("Error registering throttle_full_count metric");
-
-    static ref COUNT: IntGaugeVec =
-        register_int_gauge_vec!(
-            "throttle_count",
-            "Accumulated count of all active leases",
-            &["semaphore"]
-        ).expect("Error registering throttle_count metric");
-
-    static ref PENDING: IntGaugeVec =
-        register_int_gauge_vec!(
-            "throttle_pending",
-            "Accumulated count of all pending leases",
-            &["semaphore"]
-        ).expect("Error registering throttle_count metric");
+    static ref FULL_COUNT: IntGaugeVec = register_int_gauge_vec!(
+        "throttle_full_count",
+        "New leases which would increase the count beyond this limit are pending.",
+        &["semaphore"]
+    )
+    .expect("Error registering throttle_full_count metric");
+    static ref COUNT: IntGaugeVec = register_int_gauge_vec!(
+        "throttle_count",
+        "Accumulated count of all active leases",
+        &["semaphore"]
+    )
+    .expect("Error registering throttle_count metric");
+    static ref PENDING: IntGaugeVec = register_int_gauge_vec!(
+        "throttle_pending",
+        "Accumulated count of all pending leases",
+        &["semaphore"]
+    )
+    .expect("Error registering throttle_count metric");
 }
