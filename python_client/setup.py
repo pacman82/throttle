2d4a0c2c4c7fe8e44117e319c202d801ffa9b3ab
Markus Klein <markus-klein@live.de>
2020-03-13 00:47:53 +0100

    update meta information for python project

--- python_client/setup.py
+++ python_client/setup.py
@@ -1,10 +1,22 @@
 from setuptools import setup
 
+with open("../Readme.md", "r") as file:
+    long_description = file.read()
+
 setup(
     name="throttle_client",
-    version="0.1",
+    version="0.1.0",
+    author="Markus Klein",
     description="Client for Throttle. Throttle is a semaphore service, "
     "providing semaphores for distributed systems.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url = "https://github.com/pacman82/throttle.git",
     packages=["throttle_client"],
-    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
