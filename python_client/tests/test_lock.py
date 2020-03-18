786ba13ea08e30e53676a1afff6952a86c37c9bc
Markus Klein <markus-klein@live.de>
2020-03-18 19:15:43 +0100

    split tests in two suites

--- python_client/tests/test_lock.py
+++ python_client/tests/test_lock.py
@@ -1,50 +1,15 @@
-from throttle_client import Client, lock, Timeout
-from contextlib import contextmanager
+"""Test the clients `lock` interface."""
+
+from datetime import timedelta
 from tempfile import NamedTemporaryFile
 from threading import Thread
 from time import sleep
-from datetime import timedelta
 
 import pytest
-import requests
-import subprocess
 
-# Endpoint of throttle server started by cargo main
-BASE_URL = "http://localhost:8000"
+from throttle_client import Client, Timeout, lock
 
-
-@contextmanager
-def cargo_main(cfg: str):
-    """
-    Use as a test fixture to ensure the throttle server is running with all the
-    recent code changes.
-    """
-    # Start for server
-    proc = subprocess.Popen(["cargo", "run", "--", "-c", cfg])
-
-    try:
-        # Wait for server to boot and answer health request
-        no_connection = True
-        while proc.poll() is None and no_connection:
-            try:
-                response = requests.get(f"{BASE_URL}/health", timeout=0.2)
-                response.raise_for_status()
-                no_connection = False
-            except (requests.ConnectionError):
-                sleep(0.1)  # Sleep a tenth of a second, don't busy waits
-
-        yield proc
-    finally:
-        proc.terminate()
-
-
-@contextmanager
-def throttle_client(config: bytes):
-    with NamedTemporaryFile(delete=False) as cfg:
-        cfg.write(config)
-        cfg.close()
-        with cargo_main(cfg=cfg.name) as _:
-            yield Client(BASE_URL)
+from . import BASE_URL, cargo_main, throttle_client
 
 
 def test_error_on_leasing_unknown_semaphore():
@@ -67,45 +32,6 @@ def test_remainder():
         with lock(client, "A"):
             assert 0 == client.remainder("A")
         assert 1 == client.remainder("A")
-
-
-def test_pending_lock():
-    """
-    Verify the results of a non-blocking request to block_until_acquired
-    """
-    with throttle_client(b"[semaphores]\nA=1") as client:
-        # Acquire first lease
-        first = client.acquire("A")
-        second = client.acquire("A")
-        # Second lease is pending, because we still hold first
-        assert second.has_pending()
-        # A request to the server is also telling us so
-        assert client.block_until_acquired(second, block_for=timedelta(seconds=0))
-        client.release(first)
-        # After releasing the first lease the second lease should no longer be
-        # pending
-        assert not client.block_until_acquired(second, block_for=timedelta(seconds=0))
-
-
-def test_lock_blocks():
-    """
-    Verify that block_until_acquired blocks until the semaphore count allows for
-    the lock to be acquired.
-    """
-    with throttle_client(b"[semaphores]\nA=1") as client:
-        # Acquire first lock
-        first = client.acquire("A")
-        second = client.acquire("A")
-
-        def wait_for_second_lock():
-            client.block_until_acquired(second, block_for=timedelta(seconds=2))
-
-        t = Thread(target=wait_for_second_lock)
-        t.start()
-        client.release(first)
-        t.join()
-        # Second lock is no longer pending, because we released first and t is finished
-        assert not second.has_pending()
 
 
 def test_server_recovers_pending_lock_after_state_loss():
@@ -146,30 +72,6 @@ def test_server_recovers_pending_lock_after_state_loss():
             # during pending leases on the client side.
             t.join()
             assert acquired_lease
-
-
-def test_removal_of_expired_leases():
-    """Verify the removal of expired leases."""
-    with throttle_client(b"[semaphores]\nA=1") as client:
-        client.acquire("A", expires_in=timedelta(seconds=1))
-        sleep(2)
-        client.remove_expired()
-        assert client.remainder("A") == 1  # Semaphore should be free again
-
-
-def test_pending_leases_dont_expire():
-    """
-    Test that leases do not expire, while they wait for pending admissions.
-    """
-    with throttle_client(b"[semaphores]\nA=1") as client:
-        # Acquire once, so subsequent leases are pending
-        _ = client.acquire("A")
-        # This lease should be pending
-        peer = client.acquire("A", expires_in=timedelta(seconds=1))
-        client.block_until_acquired(peer, block_for=timedelta(seconds=1.5))
-        # The initial timeout of one second should have been expired by now,
-        # yet nothing is removed
-        assert client.remove_expired() == 0
 
 
 def test_keep_lease_alive_beyond_expiration():
