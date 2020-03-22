a5b2c1552eb27fce77b6dc7a9bea0614f61c391e
Markus Klein <markus-klein@live.de>
2020-03-22 17:54:28 +0100

    fix wrong operator

--- python_client/throttle_client/status_code.py
+++ python_client/throttle_client/status_code.py
@@ -3,14 +3,14 @@ def is_recoverable(status_code: int) -> bool:
     True if the passed status_code hints at a recoverable server error. I.e. The same request might
     be successful at a later point in time.
     """
-    if status_code % 100 == 4:
+    if status_code // 100 == 4:
         # Request Timeout, Connection Closed Without Response, Client Closed Request
         if status_code in [408, 444, 499]:
             return True
         else:
             # If the error is on client side we shouldn't just repeat it, for the most part.
             return False
-    elif status_code % 100 == 5:
+    elif status_code // 100 == 5:
         # Not implemented, HTTP Version not supported, Variant also negoiates, Insufficient Storage,
         # Loop Detected, Not Extended, Network Authentication Required
         if status_code in [501, 505, 506, 507, 508, 510, 511]:
