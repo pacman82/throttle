pacman82/throttle

commit 6317df72581b33a23ca948fab548eb1530d112d9

decouple leases and wakers

src/state.rs
@@ -135,18 +135,24 @@ impl State {
             Err(_) => unreachable!(),
         }
 
-        // Free lock. Important NOT to hold lease while polling during .await
-        drop(leases);
+        // Check if we can resolve this immediatly
+        if !leases.has_pending(peer_id)? {
+            Ok(true)
+        } else {
+            // keep holding the lock to `leases` until everything is registered. So we don't miss
+            // the call to `resolve_with`.
 
-        let acquire_or_timeout =
-            time::timeout(timeout, self.wakers.all_acquired(peer_id, &self.leases));
-        match acquire_or_timeout.await {
-            // Locks could be acquired
-            Ok(Ok(())) => Ok(true),
-            // Failure
-            Ok(Err(e)) => Err(e),
-            // Lock could not be acquired in time
-            Err(_) => Ok(false),
+            let acquire_or_timeout =
+                time::timeout(timeout, self.wakers.wait_for_resolving(peer_id));
+            // The outer `Err` indicates a timeout.
+            match acquire_or_timeout.await {
+                // Locks could be acquired
+                Ok(Ok(())) => Ok(true),
+                // Failure
+                Ok(Err(e)) => Err(e),
+                // Lock could not be acquired in time
+                Err(_) => Ok(false),
+            }
         }
     }
 

src/wakers.rs
@@ -1,4 +1,4 @@
-use crate::{error::ThrottleError, leases::Leases};
+use crate::error::ThrottleError;
 use std::{
     future::Future,
     pin::Pin,
@@ -59,22 +59,12 @@ impl Wakers {
             wakers: Mutex::new(Vec::new()),
         }
     }
-    /// Future returns, once the peer has acquired its pending lock, or the peer gets removed.
+    /// A future associated with a peer, which can be resolved using `resolve_with`.
     ///
     /// Attention: Do not call this method while holding a lock to `leases`.
-    pub async fn all_acquired(
-        &self,
-        peer_id: u64,
-        leases: &Mutex<Leases>,
-    ) -> Result<(), ThrottleError> {
-        let result = match leases.lock().unwrap().has_pending(peer_id) {
-            Ok(false) => Some(Ok(())),
-            Ok(true) => None,
-            Err(e) => Some(Err(e)),
-        };
-
+    pub async fn wait_for_resolving(&self, peer_id: u64) -> Result<(), ThrottleError> {
         let strong = Arc::new(Mutex::new(Shared {
-            result,
+            result: None,
             waker: None,
         }));
         let weak = Arc::downgrade(&strong);

commit f6de8e126f4ccdb8b80a46b9ff6a5bf4e8d6ddf6

fix deadlock introduced with
6317df72581b33a23ca948fab548eb1530d112d9

src/state.rs
@@ -144,6 +144,10 @@ impl State {
 
             let acquire_or_timeout =
                 time::timeout(timeout, self.wakers.wait_for_resolving(peer_id));
+
+            // Release lock on leases, while waiting for acquire_or_timeout! Otherwise, we might
+            // deadlock.
+            drop(leases);
             // The outer `Err` indicates a timeout.
             match acquire_or_timeout.await {
                 // Locks could be acquired