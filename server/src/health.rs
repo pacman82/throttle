50f169ef5b89543e65ce191b8374219fb123f4c7
Markus Klein <markus-klein@live.de>
2023-12-07 22:42:48 +0100

    migrate health route to axum

--- server/src/health.rs
+++ server/src/health.rs
@@ -1,22 +1,4 @@
-use actix_web::get;
-
 /// Health check used to see if server is running
-#[get("/health")]
-async fn health() -> &'static str {
+pub async fn health() -> &'static str {
     "Ok"
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-    use actix_web::{http::StatusCode, test, App};
-
-    #[actix_rt::test]
-    async fn health_route() {
-        let app = test::init_service(App::new().service(health)).await;
-        let req = test::TestRequest::with_uri("/health").to_request();
-        let resp = test::call_service(&app, req).await;
-
-        assert_eq!(resp.status(), StatusCode::OK)
-    }
 }
