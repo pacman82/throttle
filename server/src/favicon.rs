c67269798c86d05af373f676fbf61495be446334
Markus Klein <markus-klein@live.de>
2023-12-08 09:45:02 +0100

    migrate favicon to axum

--- server/src/favicon.rs
+++ server/src/favicon.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, HttpResponse};
+use axum::{response::IntoResponse, http::header};
 
 /// Favicon is statically linkend into the binary. This saves us the headache of deploying static
 /// assets.
@@ -6,9 +6,8 @@ const FAVICON: &[u8] = include_bytes!("favicon.ico");
 
 /// Browsers like to ask for an Icon. We do not want to clutter our logs with meaningless 404s so we
 /// just provide one.
-#[get("/favicon.ico")]
-pub async fn favicon() -> HttpResponse {
-    HttpResponse::Ok()
-        .content_type("image/x-icon")
-        .body(FAVICON)
+pub async fn favicon() -> impl IntoResponse {
+    (
+        [(header::CONTENT_TYPE, "image/x-icon")], FAVICON
+    )
 }
