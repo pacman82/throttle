46123061656833aaa6b5a3d8ff571578d2ee570e
Markus Klein <markus-klein@live.de>
2024-05-27 23:03:25 +0200

    list peers

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,21 +1,32 @@
-<script>
-    import { onMount } from 'svelte';
+<script lang="ts">
+	import { onMount } from 'svelte';
 
-    let backend = window.location.host;
+	//let backend = window.location.host;
+    let backend = "localhost:8000";
 
-    /**
-	 * @type {string}
-	 */
-    let version;
-    onMount(() => (
-        fetch('http://' + backend + '/version')
-        .then(response => response.text()
-            .then((v) => version = v)
-            .catch((e) => console.log(e))
-        .catch((e) => console.log(e)))))
+	let version: string;
 
+    let peers: { id: number; } [] = [];
 
+	const loadData = async () => {
+        let request_version = fetch('http://' + backend + '/version');
+        let request_peers = fetch('http://' + backend + '/peers/');
+		let response_version = await request_version;
+		version = await response_version.text();
+        let response_peers = await request_peers;
+        peers = await response_peers.json();
+	};
+
+	onMount(() => loadData().catch((e) => console.log(e)));
 </script>
 
 <h1>Throttle</h1>
 <p>Throttle version: {version}</p>
+<table>
+    <th>Peer Id</th>
+    {#each peers as peer}
+    <tr>
+        <td>{ peer.id }</td>
+    </tr>
+    {/each}
+</table>
